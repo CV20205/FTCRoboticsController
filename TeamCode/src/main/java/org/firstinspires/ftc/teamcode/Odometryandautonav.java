9445a418bb63d37e74f0c5402e3aca90f6a49945
CV20205 <20205@student.clearviewregional.edu>
2023-10-10 11:59:39 -0400

    OdomitryV1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometryandautonav.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometryandautonav.java
@@ -1,77 +1,156 @@
 package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+@Autonomous(name = "Pushbot: Auto Drive By Encoder", group = "Pushbot")
+@Disabled
 
 public class Odometryandautonav {
+    HardwarePushbot robot = new HardwarePushbot();   // Use a Pushbot's hardware
+    private ElapsedTime runtime = new ElapsedTime();
 
+    public void runOpMode() {
+
+
+        robot.init(hardwareMap);
+
+        telemetry.addData("Status", "Resetting Encoders");
+        telemetry.update();
+
+        robot.leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+        telemetry.addData("Path0", "Starting at %7d :%7d",
+                robot.leftDrive.getCurrentPosition(),
+                robot.rightDrive.getCurrentPosition());
+        telemetry.update();
+
+
+    }
+
+    static final double COUNTS_PER_MOTOR_REV = 1440;    // eg: TETRIX Motor Encoder
+    static final double DRIVE_GEAR_REDUCTION = 2.0;     // This is < 1.0 if geared UP
+    static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.77953);
+    static final double DRIVE_SPEED = 0.6;
+    static final double TURN_SPEED = 0.5;
 
     public int flocx = (1);
     public int flocy = (1);
     public int slocx = (1);
-    public int slocy = (1);
+    private int slocy = (1);
 
 
-        int meta = (flocy - slocy);
-        int steps = 14;
-        int zeta = (flocx - slocx);
-        int clocy = ((zeta * steps) + slocx);
-        int clocx = ((meta * steps) + slocy);
-        var dsn1 = Math.sqrt(((flocx * flocx) - (slocx * slocx)) + (flocy * flocy) - (slocy * slocy));
+    public int meta = (flocy - slocy);
+    public int steps = 14;
+    public int zeta = (flocx - slocx);
+    public int clocy = ((zeta * steps) + slocx);
+    public int clocx = ((meta * steps) + slocy);
+    public double dsn1 = Math.sqrt(((flocx * flocx) - (slocx * slocx)) + (flocy * flocy) - (slocy * slocy));
 
 
-                var n1x = (flocx-clocx);
-                var n1y = (flocy - clocy);
+    public double n1x = (flocx - clocx);
+    public double n1y = (flocy - clocy);
 
-                var n1yp = (flocy - clocy);
-                var n1xp = (flocx - clocx);
+    public double zet = (flocx - slocx);
+    public double met = (flocy - slocy);
 
-                function RouteAlignN1() {
-                    for (i = clocx; i < n1x; i++)
-                    for (i = clocy; i < n1y; i++)
-                    for (i = clocx; i > n1x; i--)
-                    for (i = clocy; i > n1y; i--)
-                    return run; RouteAlignN1();
+    public double blocx = (1);
+
+    public double blocy = (1);
+
+    public double n2x = (flocy - blocx);
+
+    public double n2y = (flocy - blocy);
+
+    public double dsn2 = Math.sqrt(((flocx * flocx) - (blocx * blocx)) + (flocy * flocy) - (blocy * blocy));
+    public double t = (30);
+    public double VC1 = dsn1 / t;
+    public double VC2 = dsn2 / t;
+
+    public double V = (1);
+
+
+
+
+        public int RouteAlignN1() {
+            int j = clocx;
+            int k = clocy;
+            for (double i = clocx; i < n1x; i++) {
+                j++;
+            }
+            for (double i = clocy; i < n1y; i++) {
+                k++;
+            }
+            for (double i = clocx; i > n1x; i--) {
+                j--;
+            }
+            for (double i = clocy; i > n1y; i--) {
+                k--;
+            }
+            return (k & j);
+        }
+
+            public int RouteAlignN2 () {
+                int j2 = clocx;
+                int k2 = clocy;
+                for (double i = clocx; i < n2x; i++) {
+                    j2++;
                 }
-
-                function RouteAlignN2() {
-                    for (i = clocx; i < n2x; i++)
-                    for (i = clocy; i < n2y; i++)
-                    for (i = clocx; i > n2x; i--)
-                    for (i = clocy; i > n2y; i--)
-                    return {clocx and clocy}
+                for (double i = clocy; i < n2y; i++) {
+                    k2++;
                 }
+                for (double i = clocx; i > n2x; i--) {
+                    j2--;
+                }
+                for (double i = clocy; i > n2y; i--) {
+                    k2--;
+                }
+                return (k2 & j2);
+            }
 
-                double DS = (96 * 3.14) * steps  // equation for distance traveled in mm
-                double V = DS/t  // rotation based speed calcs
-                double VC =   // current velocity of the robot
 
-            function adjustspeed() {  // alternative method to systems
-                    for(i = VC; i < VC; i++)  // does not return trajectory
-                    for(i = VC; i > VC; i--)
-                        console.print(VC) }
-    }
-            var clocx =   // x coordinate of current position
-            var clocy =   // y coordinate of current position
-            var flocx =   // x coordinate of final point
-            var flocy =   // y coordinate of final point
-            var blocx =   // x coordinate of second point
-            var blocy =   // y coordinate of second point
-            var N1x = (flocx - clocx)
-            var N1x = (flocy - clocy)
-            var N2x = (flocx - blocx)
-            var N2x = (flocy - blocy)
-            var N1 = (N1x - N1y)
-            var N2 = (N2x - N2y)
-            var Vconstant = Tx\Ty
-            var r = 96
-            const N1yP = (flocy - clocy)
-            const N1xP = (flocx - clocx)
-            const N2xP = (flocx - blocx)
-            const N2xP = (floxy - blocy)
+            public double adjustspeed () {
+                double v1 = VC1;
+                for (double i = VC1; i < V; i++) {
+                    v1++;
+                }
+                for (double i = VC1; i > V; i--) {
+                    v1--;
+                }
+                while (VC1 == V) {
+                    RouteAlignN1();
+                }
+                double VCf = v1;
+                return VCf;
+            }
 
-                var steps = 4
-                var zet = (flocx - slocx)
-                var met = (flocy - slocy)
+        }
 
-                var clocx = ((zet * steps) + slocx)
-                var clocy = ((met * steps) + slocy)
 
-                var zet = (flocx - slocx)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
